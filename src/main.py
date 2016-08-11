6f63b35fcff24eefe8b28a8d39238c4e0de2d155
cody <kochmanncody@gmail.com>
2016-08-11 15:11:07 -0400

    - cleaned up the code with pylint to make as much of it understandable as I can

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 # @Author: cody
 # @Date:   2016-07-21 08:13:01
 # @Last Modified 2016-08-11
-# @Last Modified time: 2016-08-11 14:10:23
+# @Last Modified time: 2016-08-11 15:10:24
 
 """
 This runs analysis over binary patterns to find similarities in
@@ -48,40 +48,43 @@ def calculate_similarity(l_one, l_two):
         similar = 0
         for i in range(len(l_two)):
             if l_one[i+offset] is l_two[i]:
-                similar+=1
+                similar += 1
         if similar > best_score:
             best_score = similar
     return best_score / float(len(l_one))
 
-def binary_str_to_pattern(i):
+def binary_str_to_pattern(binary_string):
     """ returns the opposite of binary_pattern_to_str() """
-    if isinstance(i, list):
-        return i
+    if isinstance(binary_string, list):
+        return binary_string
     out = deque()
-    for x in i:
-        out.append(x.upper() == 'T')
+    for character in binary_string:
+        out.append(character.upper() is 'T')
     return list(out)
 
 
-def binary_pattern_to_str(i):
-    if isinstance(i, basestring):
-        return i
+def binary_pattern_to_str(binary_pattern):
+    """ converts binary patterns into strings """
+    if isinstance(binary_pattern, basestring):
+        return binary_pattern
     out = ''
-    for x in i:
-        if x:
+    for node in binary_pattern:
+        if node:
             out += 'T'
         else:
             out += 'F'
     return out
 
 
-class Binary_Pattern_Analysis(object):
+class BinaryPatternAnalysis(object):
     """analyzes binary patterns given to it"""
-
     def __init__(self):
         self.pattern_collection = {}
 
-    def add(self, binary_pattern):  # takes patterns like [True,False,False]
+    def add(self, binary_pattern):
+        """ use this function to collect binary patterns
+            takes patterns like [True,False,False]
+        """
         binary_str = binary_pattern_to_str(binary_pattern)
         if binary_str in self.pattern_collection:
             self.pattern_collection[binary_str] += 1
@@ -104,13 +107,14 @@ class Binary_Pattern_Analysis(object):
         binary_str = binary_pattern_to_str(binary_pattern)
         for k in self.pattern_collection:
             if binary_str in k:
-                out[k] = self.binary_pattern[k]
+                out[k] = self.pattern_collection[k]
         return out
 
-    def count_patterns_at_length(self, l):
+    def count_patterns_at_length(self, input_length):
+        """ counts the binary_patterns that match a certain length """
         out = 0
         for k in self.pattern_collection:
-            if len(k) == l:
+            if len(k) is input_length:
                 out += 1
         return out
 
@@ -151,13 +155,14 @@ class Binary_Pattern_Analysis(object):
 
 
 
-class Binary_Tangent(object):
+class BinaryTangent(object):
+    """ object that turns a binary array into a binary tree to be analyzed """
     def __init__(self, children=[], parent=None):
         """
             r = child node if its true
             l = child node if its false
         """
-        assert isinstance(children, list), "children needs to be a string: {}".format(children)
+        assert isinstance(children, list), "children needs to be a list: {}".format(children)
         self.r = None
         self.l = None
         if len(children):
@@ -172,30 +177,32 @@ class Binary_Tangent(object):
             self.depth = 0
         if len(children):
             if children[0]: # if the next child is True
-                self.r = Binary_Tangent(children, self)
+                self.r = BinaryTangent(children, self)
             else: # if the next child is False
-                self.l = Binary_Tangent(children, self)
+                self.l = BinaryTangent(children, self)
 
     def get_root(self):
-        # noinspection PyShadowingNames
-        t = self
-        while t.parent is not None:
+        """ returns the first node in a tangent """
+        node = self
+        while node.parent is not None:
             # noinspection PyShadowingNames
-            t = t.parent
-        return t
+            node = node.parent
+        return node
 
     def get_last_child(self):
-        # noinspection PyShadowingNames
-        t = self
-        while t.has_child():
+        """ returns the last node in a tangent """
+        node = self
+        while node.has_child():
             # noinspection PyShadowingNames
-            t = t.get_child()
-        return t
+            node = node.get_child()
+        return node
 
     def has_child(self):
+        """ boolean test if the node has a child """
         return self.r is not None or self.l is not None
 
     def get_child(self):
+        """ returns a node's child if it has one """
         if self.has_child():
             if self.r is not None:
                 return self.r
@@ -206,7 +213,7 @@ class Binary_Tangent(object):
 
     @staticmethod
     def count_nodes(node):
-        # noinspection PyShadowingNames
+        """ returns the size of the tangent """
         if node.parent is not None:
             node = node.get_root()
         out = 1
@@ -217,51 +224,61 @@ class Binary_Tangent(object):
             node = node.get_child()
 
     @staticmethod
-    def child_patterns(node,min_length,max_length):
+    def child_patterns(node, min_length, max_length):
+        """ recursive pattern generation of all child nodes """
         pattern = deque(maxlen=max_length)
         try:
             while True:
                 pattern.append(node.val)
-                if len(pattern)>=min_length:
+                if len(pattern) >= min_length:
                     yield list(pattern)
-                node=node.get_child()
+                node = node.get_child()
         except AttributeError, e:
+            # attribute errors are thrown when the next child is None
             pass
 
     @staticmethod
     def all_children(root_node):
+        """ yeilds every child node of the currently using this function """
         while root_node.has_child():
             root_node = root_node.get_child()
             yield root_node
 
     @staticmethod
-    def all_child_patterns(root_node,pattern_size_limit):
+    def all_child_patterns(root_node, pattern_size_limit):
+        """ yeilds every pattern that is beneath the current node """
         min_pattern_depth = 3
         for node in root_node.all_children(root_node):
             for pattern in root_node.child_patterns(node, min_pattern_depth, pattern_size_limit):
                 yield pattern
 
     def find_patterns(self, pattern_size_limit=0):
+        """ generates a pattern analysis of the tangent's nodes """
         root_node = self.get_root()
-        analysis = Binary_Pattern_Analysis()
+        analysis = BinaryPatternAnalysis()
         node_count = self.count_nodes(self)
         if node_count <= pattern_size_limit or pattern_size_limit == 0:
             pattern_size_limit = node_count - 1
-        for pattern in self.all_child_patterns(root_node,pattern_size_limit):
+        for pattern in self.all_child_patterns(root_node, pattern_size_limit):
             analysis.add(pattern)
         return analysis
 
     def last_nodes(self, node_count):
+        """ returns the last nodes in the tangent
+            basically, an equivelent to str[:-5]
+        """
         if node_count > self.count_nodes(self):
             node_count = self.count_nodes(self)
-        t = self.get_last_child()
+        node = self.get_last_child()
         out = []
-        while node_count > len(out):
-            out.append(t.val)
-            t = t.parent
+        for _ in range(node_count):
+            out.append(node.val)
+            node = node.parent
         return out
 
-    def calculate_next(self, nodes_to_consider=6):  # I chose 6 since humans chose 7 to process reliably
+    def calculate_next(self, nodes_to_consider=6):
+        """ This calculates how likely the tangent's next node will be True
+        I chose 6 as default since human brains chose 7 to process reliably """
         if nodes_to_consider > (self.count_nodes(self) / 2):
             nodes_to_consider = self.count_nodes(self) / 2
         binary_pattern = self.last_nodes(nodes_to_consider)
@@ -304,6 +321,10 @@ class Binary_Tangent(object):
         # predictions = {'TF': 1, 'TFF': 1, 'T': 2}
         scores = [0.0] * longest_key(predictions)
         datapoints = 0
+        # this mess of a loop below basically does this
+        # {'TF': 1, 'TFF': 1, 'T': 2}  -->  [3, -2, -1]
+        # this gives us a index of true's that have been
+        # seen in the past to come after the current tangent
         for i in range(longest_key(predictions)):
             for k in predictions:
                 if len(k) > i:
@@ -314,10 +335,17 @@ class Binary_Tangent(object):
                         if k[i] == 'F':
                             datapoints += 1
                             scores[i] -= 1.0
+        # This snippet levels out the score's momentum. What
+        # this portion was designed for was to smooth out the
+        # change to let it understand what will come next:
+        #   up 3, down 2, down 2 is the same as 3, 1, 0.
+        # so
+        #   score[3, -2, -1]  -->  score[3, 1, 0]
         for i in range(len(scores)):
             if i > 0:
                 scores[i] += scores[i - 1]
         final_score = (scores[-1] + datapoints) / (datapoints * 2)
+        # this was being used to test the algorithm when I was first designing it.
         # print 'collected previous conclusions to this moment:\n{}'.format(predictions)
         # print 'score progression: {}'.format(scores)
         # print '========================================================='
@@ -328,9 +356,9 @@ class Binary_Tangent(object):
             "similar_patterns": int(sum(predictions.values()))
         }
 
-    """===================================================================
-        Everything below this is crap for rendering the Binary_Tangent
-    ==================================================================="""
+    # ======================================================================
+    #     Everything below this is crap for rendering the BinaryTangent
+    # ======================================================================
 
     @staticmethod
     def root_indent(node):
@@ -357,9 +385,11 @@ class Binary_Tangent(object):
         while depth:
             depth -= 1
             node = node.get_child()
-            if node.val == True:
+            # dont optimize this. Anything other than explicitly true or false
+            # cannot be a value that is acknowledged here.
+            if node.val is True:
                 indent += 2
-            if node.val == False:
+            elif node.val is False:
                 indent -= 2
         return indent
 
@@ -371,13 +401,13 @@ class Binary_Tangent(object):
         return node.indent_from_root(node)
 
     def render(self):
-        """ recursively renders the entire Binary_Tangent in the CLI """
+        """ recursively renders the entire BinaryTangent in the CLI """
         if self.val:
-            s = "T"
+            string_form = "T"
         else:
-            s = "F"
+            string_form = "F"
         indent = self.get_indent(self)
-        print ' ' * indent + s
+        print ' ' * indent + string_form
         brackets = ''
         if self.l is not None:
             brackets += '/ '
@@ -398,22 +428,21 @@ class Binary_Tangent(object):
 
     @staticmethod
     def gen_pattern_tree():
-        """ this will convert the Binary_Tangent into a Pattern_Tree """
+        """ this will convert the BinaryTangent into a Pattern_Tree """
         pass
 
 
-
 def run_test():
-    t = Binary_Tangent(random_binary_pattern(128))
-    t.render()
-    patterns = t.find_patterns()
-    collection = patterns.pattern_collection
+    """ this runs a test over a majority of the code to test it. """
+    tangent = BinaryTangent(random_binary_pattern(256))
+    tangent.render()
+    patterns = tangent.find_patterns()
     line()
     print 'Collected Patterns:'
     patterns.display_patterns()
     line()
     print 'Calculating a prediction for the next value...'
-    print t.calculate_next()
+    print tangent.calculate_next()
 
 if __name__ == '__main__':
     run_test()
